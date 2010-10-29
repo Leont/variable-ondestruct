4cf781921b1fcda57ecea8c96ff13b7d2aa32d6c
Leon Timmermans <fawaka@gmail.com>
2010-10-30 00:40:34 +0300

    Updated documentation

--- lib/Variable/OnDestruct.pm
+++ lib/Variable/OnDestruct.pm
@@ -1,7 +1,7 @@
 package Variable::OnDestruct;
 
 use strict;
-use warnings;
+use warnings FATAL => 'all';
 use Exporter 5.57 'import';
 use XSLoader;
 
@@ -33,10 +33,12 @@ Version 0.03
 	on_destruct $var, sub { do_something() };
 	on_destruct @array, sub { do_something_else() };
 	on_destruct %array, sub { hashes_work_too() };
+	on_destruct &$sub, sub { so_do_closures($but_not_normal_subs) };
+	on_destruct *$glob, sub { and_even_globs($similar_caveats_as_subs_though) };
 
 =head1 DESCRIPTION
 
-This module allows you to let a function be called when a variable gets destroyed. The destructor will work not only on scalars but also on arrays and hashes.
+This module allows you to let a function be called when a variable gets destroyed. The destructor will work not only on scalars but also on arrays, hashes, subs and globs. For the latter two you should realize that most of them aren't scoped like normal variables. Subs for example will only work like you expect them to when they are closures.
 
 =head1 FUNCTIONS
 
@@ -88,7 +90,7 @@ L<http://search.cpan.org/dist/Variable-OnDestruct>
 
 =head1 COPYRIGHT & LICENSE
 
-Copyright 2009 Leon Timmermans, all rights reserved.
+Copyright 2009,2010 Leon Timmermans, all rights reserved.
 
 This program is free software; you can redistribute it and/or modify it
 under the same terms as Perl itself.
